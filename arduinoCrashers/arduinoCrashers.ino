23387ca3aecadd44ddcc045968200abee2dba4f9
Andrea <andrea.rosati87@yahoo.it>
2019-12-08 19:27:07 +0100

    Gestione led e cambio capitolo

--- arduinoCrashers/arduinoCrashers.ino
+++ arduinoCrashers/arduinoCrashers.ino
@@ -1,8 +1,8 @@
 #include "Adafruit_Thermal.h"
 #include "SoftwareSerial.h"
 #include <Servo.h>
-#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
-#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+#define TX_PIN 4 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 3 // Arduino receive   GREEN WIRE   labeled TX on printer
 SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
 Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
 
@@ -16,18 +16,22 @@ const int heroPin = 8;
 const int enemyPin = 9;
 const int princessPin = 10;
 
-const int yesButtonPin = 3;
-const int noButtonPin = 4;
+const int yesButtonPin = 5;
+const int noButtonPin = 6;
 
 const int basePosition = 0;
 const int walkPosition = 90;
 const int defensePosition = 180;
 const int attackPosition = 270;
 
-int heroLife[3] = {11, 12, 13};
-int enemyLife[3] = {14, 15, 16};
+int heroLifeLedPin[3] = {11, 12, 13};
+int enemyLifeLedPin[3] = {14, 15, 16};
+
+int heroLife = 3;
+int enemyLife = 3;
+
 enum  story {
-  INIT, GOINGTOSAVEPRINCESS, GOINGEQUALLY, HEROICENTRY, STEALTHENTRY, ENEMYDEFENSE, ENEMYSURPISED, ENEMYATTACHED, YOUDIE, YOUWIN
+  SETUP, INIT, GOINGTOSAVEPRINCESS, GOINGEQUALLY, HEROICENTRY, STEALTHENTRY, ENEMYDEFENSE, ENEMYSURPISED, ENEMYATTACHED, YOUDIE, YOUWIN
 };
 story chapter = INIT;
 
@@ -37,7 +41,7 @@ void setup() {
 
   // This line is for compatibility with the Adafruit IotP project pack,
   // which uses pin 2 as a spare grounding point.  You only need this if
-  // wired up the same way (w/3-pin header into pins 5/6/2):
+  // wired up the same way (w/3-pin header into pins 4/3/2):
   pinMode(2, OUTPUT); digitalWrite(2, LOW);
 
   // NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
@@ -47,21 +51,21 @@ void setup() {
   pinMode(yesButtonPin, INPUT);
   pinMode(noButtonPin, INPUT);
 
-  pinMode(heroLife[0], OUTPUT);
-  pinMode(heroLife[1], OUTPUT);
-  pinMode(heroLife[2], OUTPUT);
+  pinMode(heroLifeLedPin[0], OUTPUT);
+  pinMode(heroLifeLedPin[1], OUTPUT);
+  pinMode(heroLifeLedPin[2], OUTPUT);
 
-  digitalWrite(heroLife[0], HIGH);
-  digitalWrite(heroLife[1], HIGH);
-  digitalWrite(heroLife[2], HIGH);
+  digitalWrite(heroLifeLedPin[0], HIGH);
+  digitalWrite(heroLifeLedPin[1], HIGH);
+  digitalWrite(heroLifeLedPin[2], HIGH);
 
-  pinMode(enemyLife[0], OUTPUT);
-  pinMode(enemyLife[1], OUTPUT);
-  pinMode(enemyLife[2], OUTPUT);
+  pinMode(enemyLifeLedPin[0], OUTPUT);
+  pinMode(enemyLifeLedPin[1], OUTPUT);
+  pinMode(enemyLifeLedPin[2], OUTPUT);
 
-  digitalWrite(enemyLife[0], HIGH);
-  digitalWrite(enemyLife[1], HIGH);
-  digitalWrite(enemyLife[2], HIGH);
+  digitalWrite(enemyLifeLedPin[0], HIGH);
+  digitalWrite(enemyLifeLedPin[1], HIGH);
+  digitalWrite(enemyLifeLedPin[2], HIGH);
 
   heroServo.attach(8);
   enemyServo.attach(9);
@@ -116,6 +120,12 @@ void loop() {
         break;
       }
 
+    case SETUP:
+      {
+        setup();
+        break;
+      }
+
     default:
       {
         break;
@@ -128,8 +138,31 @@ void loop() {
 
 
 
-void changeChapter(int hero,int enemy,int princess,story newChapter)
+void changeChapter(int hero, int enemy, int princess, story newChapter)
 {
-
+  heroServo.write(hero);
+  enemyServo.write(enemy);
+  princessServo.write(princess);
+  chapter = newChapter;
   return;
+}
+
+bool heroLoseLife()
+{
+  heroLife--;
+  digitalWrite(heroLifeLedPin[heroLife], LOW);
+  if (heroLife == 0)
+    return true;
+
+  return false;
+}
+
+bool enemyLoseLife()
+{
+  enemyLife--;
+  digitalWrite(enemyLifeLedPin[enemyLife], LOW);
+  if (enemyLoseLife == 0)
+    return true;
+
+  return false;
 }
